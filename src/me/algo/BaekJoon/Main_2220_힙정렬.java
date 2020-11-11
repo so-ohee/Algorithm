02588c7324631d22d325eb040b0916a36b6310b3
sohee <sohee960328@gmail.com>
2020-11-11 17:26:52 +0900

    2020 11 11

---
+++ src/me/algo/BaekJoon/Main_2220_힙정렬.java
@@ -0,0 +1,21 @@
+package me.algo.BaekJoon;
+
+import java.util.Scanner;
+
+public class Main_2220_힙정렬 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int[] heap = new int[n+1];
+        for(int i=1; i<n; i++){
+            for(int j=i; j>1; j/=2) {
+                heap[j] = heap[j / 2];
+            }
+            heap[1] = i+1;
+        }
+        heap[n]=1;
+        for(int i=1; i<heap.length; i++)
+            System.out.print(heap[i]+" ");
+    }
+}
