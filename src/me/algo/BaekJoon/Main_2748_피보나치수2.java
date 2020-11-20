64cb72f68079c69ed9b663b563407e195de7015a
sohee <sohee960328@gmail.com>
2020-11-20 23:27:05 +0900

    2020 11 20

---
+++ src/me/algo/BaekJoon/Main_2748_피보나치수2.java
@@ -0,0 +1,20 @@
+package me.algo.BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_2748_피보나치수2 {
+    static long d[] = new long[91];
+    public static long fibonacci(int n){
+        if(n==0) return 0;
+        else if(n==1) return 1;
+
+        if(d[n]!=0) return d[n];
+        return d[n] = fibonacci(n-1)+fibonacci(n-2);
+    }
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fibonacci(n));
+    }
+}
