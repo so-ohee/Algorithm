e02038f3efc7db19ebd6d1364225b3eb7d1dce18
sohee <sohee960328@gmail.com>
2020-12-07 21:40:24 +0900

    2020 12 07

---
+++ src/me/algo/BaekJoon/Main_7568_덩치.java
@@ -0,0 +1,33 @@
+package me.algo.BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* 덩치
+ * 2020. 12. 07
+ * https://www.acmicpc.net/problem/7568
+ */
+
+public class Main_7568_덩치 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][3];
+        for(int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[i][0]<arr[j][0]&&arr[i][1]<arr[j][1])
+                    arr[i][2]++;
+                else if(arr[i][0]>arr[j][0]&&arr[i][1]>arr[j][1])
+                    arr[j][2]++;
+            }
+        }
+        for(int i=0; i<n; i++)
+            System.out.print((arr[i][2]+1)+" ");
+    }
+}
