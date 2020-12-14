66fe495d76613a3046e9adac048b5393ac589511
sohee <sohee960328@gmail.com>
2020-12-14 17:25:43 +0900

    update 2020 12 14

--- src/me/algo/BaekJoon/Main_1065_한수.java
+++ src/me/algo/BaekJoon/Main_1065_한수.java
@@ -1,35 +1,32 @@
 package me.algo.BaekJoon;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 /* 한수
- * 2020. 11. 27
+ * 2020. 12. 14
  * https://www.acmicpc.net/problem/1065
  */
 
 public class Main_1065_한수 {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int hansu(int n) {
+        if (n < 100) return n;
+        else {
+            int answer = 0;
+            if (n == 1000) n = 999;
+            for (int i = 100; i <= n; i++) {
+                int a = i % 10;
+                int b = (i / 10) % 10;
+                int c = (i / 100) % 10;
 
-        int n = Integer.parseInt(br.readLine()), k, han=0, a[] = new int[3];
-        for (int i = 1; i <= n; i++) {
-            if (0 < i && i < 100) han = i;
-            else if (i == 1000) break;
-            else {
-                k = 0; int t = i;
-                while (t > 0) {
-                    a[k] = t % 10;
-                    t /= 10; k++;
-                }
-                if (a[0] - a[1] == a[1] - a[2]) han++;
+                if (a - b == b - c) answer++;
             }
+            return answer + 99;
         }
-        bw.write(String.valueOf(han));
-        bw.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(hansu(Integer.parseInt(br.readLine())));
     }
 }
-
