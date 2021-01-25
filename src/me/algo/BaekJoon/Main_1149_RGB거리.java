74857dc6dfe9e6b1fbde4275e2c514e80f06dee7
sohee <sohee960328@gmail.com>
2021-01-25 14:27:57 +0900

    2021 01 25

--- src/me/algo/BaekJoon/Main_1149_RGB거리.java
+++ src/me/algo/BaekJoon/Main_1149_RGB거리.java
@@ -1,33 +1,32 @@
 package me.algo.BaekJoon;
 
-/* RGB거리
- * 2021. 01. 18
- * https://www.acmicpc.net/problem/1149
- */
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/* RGB거리
+ * 2021. 01. 25
+ * https://www.acmicpc.net/problem/1149
+ */
 public class Main_1149_RGB거리 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[3];
+        int[] dp = new int[3];
         for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
             if(i==0){
-                arr[0] = Integer.parseInt(st.nextToken());
-                arr[1] = Integer.parseInt(st.nextToken());
-                arr[2] = Integer.parseInt(st.nextToken());
+                dp[0] = Integer.parseInt(st.nextToken());
+                dp[1] = Integer.parseInt(st.nextToken());
+                dp[2] = Integer.parseInt(st.nextToken());
             }else{
-                int a=arr[0], b=arr[1];
-                arr[0] = Math.min(b,arr[2])+Integer.parseInt(st.nextToken());
-                arr[1] = Math.min(a,arr[2])+Integer.parseInt(st.nextToken());
-                arr[2] = Math.min(a,b)+Integer.parseInt(st.nextToken());
+                int R=dp[0], G=dp[1];
+                dp[0] = Math.min(G,dp[2])+Integer.parseInt(st.nextToken());
+                dp[1] = Math.min(R,dp[2])+Integer.parseInt(st.nextToken());
+                dp[2] = Math.min(R,G)+Integer.parseInt(st.nextToken());
             }
         }
-        System.out.println(Math.min(arr[0],Math.min(arr[1],arr[2])));
+        System.out.println(Math.min(dp[0],Math.min(dp[1],dp[2])));
     }
 }
