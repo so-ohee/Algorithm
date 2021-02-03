92a2b2403445cc5d322b62802a6bfede2232642f
sohee <sohee960328@gmail.com>
2021-02-03 16:44:36 +0900

    2021 02 03

--- src/me/algo/BaekJoon/Main_9461_파도반수열.java
+++ src/me/algo/BaekJoon/Main_9461_파도반수열.java
@@ -1,7 +1,7 @@
 package me.algo.BaekJoon;
 
 /* 파도반 수열
- * 2021. 01. 18
+ * 2021. 02. 03
  * https://www.acmicpc.net/problem/9461
  */
 
@@ -14,13 +14,26 @@ public class Main_9461_파도반수열 {
         if(dp[n]!=0) return dp[n];
         return dp[n]=solution(n-2)+solution(n-3);
     }
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        dp[1] = dp[2] = dp[3] = 1;
         int T = Integer.parseInt(br.readLine());
-        dp[1]=dp[2]=dp[3]=1;
-        for(int i=0; i<T; i++){
-            int n = Integer.parseInt(br.readLine());
-            System.out.println(solution(n));
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(solution(N)).append("\n");
         }
+        System.out.println(sb.toString());
     }
+
+// 2021. 01. 18
+//    public static void main(String[] args) throws Exception{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int T = Integer.parseInt(br.readLine());
+//        dp[1]=dp[2]=dp[3]=1;
+//        for(int i=0; i<T; i++){
+//            int n = Integer.parseInt(br.readLine());
+//            System.out.println(solution(n));
+//        }
+//    }
 }
