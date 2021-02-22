cbe44d4eba2a8e77139ec9f90c8c469693d739f3
sohee <sohee960328@gmail.com>
2021-02-22 14:30:50 +0900

    2021 02 22

---
+++ src/me/algo/BaekJoon/Main_15651_N과M_3.java
@@ -0,0 +1,39 @@
+package me.algo.BaekJoon;
+
+/* N과 M (3)
+ * 2021. 02. 22
+ * https://www.acmicpc.net/problem/15651
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_15651_N과M_3 {
+    static int N,M;
+    static int arr[] = new int[9];
+    static StringBuilder sb = new StringBuilder();
+
+    private static void dfs(int count){
+        if(count==M){
+            for(int i=0; i<M; i++)
+                sb.append(arr[i]).append(' ');
+            sb.append('\n');
+            return;
+        }
+        for(int i=1; i<=N; i++){
+            arr[count]=i;
+            dfs(count+1);
+        }
+    }
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        dfs(0);
+        System.out.println(sb);
+    }
+}
