22cfb3bb30844eb3c9955e4175f2883241184894
sohee <sohee960328@gmail.com>
2021-02-25 13:53:28 +0900

    2021 02 25

---
+++ src/me/algo/BaekJoon/Main_14889_스타트와링크.java
@@ -0,0 +1,63 @@
+package me.algo.BaekJoon;
+
+/* 스타트와 링크
+ * 2021. 02. 24
+ * https://www.acmicpc.net/problem/14889
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main_14889_스타트와링크 {
+    static int N;
+    static int[][] arr;
+    static boolean[] visit;
+    static int min = Integer.MAX_VALUE;
+    private static void dfs(int index, int count){
+        if(count == N/2){
+            int start=0, link=0;
+            for(int i=0; i<N-1; i++){
+                for(int j=i+1; j<N; j++){
+                    if(visit[i]==true && visit[j]==true){
+                        start += (arr[i][j]+arr[j][i]);
+                    }else if(visit[i]==false && visit[j]==false){
+                        link += (arr[i][j]+arr[j][i]);
+                    }
+                }
+            }
+            min = Math.min(min,Math.abs(start-link));
+            if(min==0){
+                System.out.println(min);
+                System.exit(0);
+            }
+            return;
+        }
+        
+        for(int i=index; i<N; i++){
+            if(!visit[i]){
+                visit[i]=true;
+                dfs(i+1,count+1);
+                visit[i]=false;
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+        visit = new boolean[N];
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        dfs(0,0);
+        System.out.println(min);
+    }
+}
