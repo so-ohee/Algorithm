dd53df025917f7bb1e88958d177970deee5e5cd5
sohee <sohee960328@gmail.com>
2021-03-29 17:54:39 +0900

    2021 03 29

---
+++ src/me/algo/Programmers/level1/M_72410_신규아이디추천.java
@@ -0,0 +1,48 @@
+package me.algo.Programmers.level1;
+
+/* 신규 아이디 추천
+ * 2021. 03. 29
+ * https://programmers.co.kr/learn/courses/30/lessons/72410
+ */
+
+public class M_72410_신규아이디추천 {
+    public static String solution(String new_id) {
+        //1단계
+        new_id = new_id.toLowerCase();
+
+        //2단계
+        new_id = new_id.replaceAll("[^-_.a-z0-9]", "");
+
+        //3단계
+        new_id = new_id.replaceAll("[.]{2,}",".");
+
+        //4단계
+        if(new_id.startsWith("."))
+            new_id = new_id.substring(1);
+        if(new_id.endsWith("."))
+            new_id = new_id.substring(0,new_id.length()-1);
+
+        //5단계
+        if(new_id.length()==0)
+            new_id = "a";
+
+        //6단계
+        if(new_id.length()>15)
+            new_id = new_id.substring(0,15);
+        if(new_id.endsWith("."))
+            new_id = new_id.substring(0,new_id.length()-1);
+
+        //7단계
+        if(new_id.length()<3){
+            String s = new_id.charAt(new_id.length()-1)+"";
+            while (new_id.length()<3){
+                new_id += s;
+            }
+        }
+        return new_id;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("abcdefghijklmn.p"));
+    }
+}
