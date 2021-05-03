b0acf0035e09ecb279e59a137d1b284315dbdb7a
sohee <sohee960328@gmail.com>
2021-05-03 14:22:13 +0900

    2021 05 03

--- src/me/algo/BaekJoon/TwoPointers/Main_3273_두수의합.java
+++ src/me/algo/BaekJoon/TwoPointers/Main_3273_두수의합.java
@@ -1,4 +1,4 @@
-package me.algo.BaekJoon;
+package me.algo.BaekJoon.TwoPointers;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /* 두수의 합
+ * 2021. 05. 03
  * 2021. 01. 10
  * https://www.acmicpc.net/problem/3273
  */
@@ -29,6 +30,7 @@ public class Main_3273_두수의합 {
             if(sum==x) {
                 count++;
                 left++;
+                right--;
             }else if(sum>x)
                 right--;
             else
